2f5795412692ab3950857a1726555c1761cdfb60
man92C <am.mann@hotmail.com>
2025-12-17 12:03:23 -0500

    task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -76,6 +76,31 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   
     return res.status(200).json({ message: "Review successfully added/modified", reviews: book.reviews });
   });
+
+  // Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization && req.session.authorization.username;
+  
+    // 1. prüfen, ob eingeloggt
+    if (!username) {
+      return res.status(401).json({ message: "User not logged in" });
+    }
+  
+    const book = books[isbn];
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  
+    if (!book.reviews || !book.reviews[username]) {
+      return res.status(404).json({ message: "Review by this user not found" });
+    }
+  
+    // 2. Review dieses Users löschen
+    delete book.reviews[username];
+  
+    return res.status(200).json({ message: "Review deleted", reviews: book.reviews });
+  });  
   
 
 module.exports.authenticated = regd_users;
