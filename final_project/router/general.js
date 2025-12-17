bd659452e483150023c8c0df789c6b0132915558
man92C <am.mann@hotmail.com>
2025-12-17 10:55:14 -0500

    getbook by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,21 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const result = [];
+
+  for(const isbn in books){
+    const book = books[isbn];
+    if (book.author === author) {
+        result.push(book);
+    }
+  }
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  } else {
+    return resizeTo.status(404).json({message: "Author not found"});
+  }
 });
 
 // Get all books based on title
