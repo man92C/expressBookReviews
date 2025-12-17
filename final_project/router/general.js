cbaf4ef52e34ae4e133da3f65bbf7fc06fa7db6d
man92C <am.mann@hotmail.com>
2025-12-17 11:23:12 -0500

    task 6 completed code for registering a new user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,22 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username || !password) {
+    return res.status(400).json({message: "Username and password are required"});
+  }
+
+  const userExists = users.some((user) => user.username === username);
+
+  if(userExists){
+    return res.status(409).json({message: "User already exists"});
+  }
+
+  users.push({username,password});
+
+  return res.status(200).json({message: "User successfully registered. Now you can login"});
 });
 
 // Get the book list available in the shop
@@ -60,7 +75,7 @@ public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
 
-  const book = book[isbn];
+  const book = books[isbn];
 
   if(book && book.reviews){
     return res.status(200).json(book.reviews);
