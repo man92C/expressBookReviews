8f8ce844193ec3d3653b693eb0ab22491ef6de97
man92C <am.mann@hotmail.com>
2025-12-17 11:09:19 -0500

    getbookreview task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,7 +58,17 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  const book = book[isbn];
+
+  if(book && book.reviews){
+    return res.status(200).json(book.reviews);
+  } else if (book) {
+    return res.status(200).json({});
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.general = public_users;
